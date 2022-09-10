bbd0727aa478371f78dc36eade6a7efb496e0b55
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-09-10 19:16:09 -0300

    Initial commit

---
+++ _Django-Blog/MyBlog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Myblog(models.Model):
+    subject = models.CharField(max_length=200)
+    date = models.DateField(default=timezone.now)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    description = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    images = models.ImageField()
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.category
